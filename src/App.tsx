5a02d76f7ea25b9e5320f7f3d4741cd56b0da610
Javier <jalcoser1996@gmail.com>
2021-05-24 17:42:02 -0500

    Actualizado proyecto inicial

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <div>
       <h1>React + Electron </h1>
+      <h1>Modo Dev</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cum autem earum omnis culpa dolor quibusdam corrupti vero adipisci, ratione, ullam placeat natus cumque necessitatibus quasi praesentium odit? Officiis, facere?</p>
- 
+      <h2>Lorem, ipsum dolor sit <strong>amet consectetur adipisicing elit. Eligendi ea</strong> reprehenderit nulla quam molestias explicabo sunt fugit recusandae neque iste consequuntur in quod perspiciatis, saepe fugiat. Aperiam cupiditate beatae expedita.</h2>
     </div>
   );
 }
