596adce5c00506cbc7cb97a0c39b2da001b188e1
Javier <jalcoser1996@gmail.com>
2021-05-24 17:17:16 -0500

    Proyecto Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React + Electron </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cum autem earum omnis culpa dolor quibusdam corrupti vero adipisci, ratione, ullam placeat natus cumque necessitatibus quasi praesentium odit? Officiis, facere?</p>
+ 
     </div>
   );
 }
